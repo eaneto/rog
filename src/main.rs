6581bf2674320c2c7a531c4ff13e4f8bb159ea96
Edison Neto <edison.aguiar.neto@gmail.com>
2023-05-11 22:48:26 -0300

    Add initial dependencies and initial main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+use log::info;
+
+#[derive(Parser, Debug)]
+struct Args {
+    /// Port to run the server
+    #[arg(short, long, default_value_t = 7878)]
+    port: u16,
+}
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+    let args = Args::parse();
+    info!("Running rog on port {}", args.port);
 }
