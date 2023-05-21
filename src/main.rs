595bef284dc433902740fba6400010699335c380
Edison Neto <edison.aguiar.neto@gmail.com>
2023-05-20 23:39:45 -0300

    Add simple initial data structures and TODO list

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::time::Instant;
+
+use bytes::Bytes;
 use clap::Parser;
 use tracing::info;
 
@@ -8,9 +11,39 @@ struct Args {
     port: u16,
 }
 
+struct _Message {
+    log: String,
+    partition: Option<usize>,
+    payload: Bytes,
+    timestamp: Instant,
+}
+
+#[derive(Debug)]
+struct CommitLog {
+    _name: String,
+    _partitions: usize,
+}
+
+impl CommitLog {
+    fn _new(name: String, partitions: usize) -> CommitLog {
+        CommitLog {
+            _name: name,
+            _partitions: partitions,
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
     let args = Args::parse();
     info!(port = args.port, "Running rog");
+
+    // TODO Create log command
+    // TODO Produce to log
+    // TODO Subscribe to log
+    // TODO Enable multiple subscribers to same log
+    // TODO Distribute writes to multiple nodes
+
+    loop {}
 }
