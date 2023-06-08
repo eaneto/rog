80588e853e73d6b4c1b37338c61d5fcfd4bbd4e6
Edison Neto <edison.aguiar.neto@gmail.com>
2023-06-07 22:40:56 -0300

    Reestructure code extracting command and log modules (#4)

---
+++ src/log.rs
@@ -0,0 +1,159 @@
+use std::{collections::HashMap, env, sync::Arc, time::SystemTime};
+
+use bytes::BytesMut;
+use serde::{Deserialize, Serialize};
+use tokio::{
+    fs::{self, File, OpenOptions},
+    io::AsyncWriteExt,
+    sync::{
+        mpsc::{self, Receiver, Sender},
+        RwLock,
+    },
+};
+use tracing::{debug, error};
+
+#[derive(Debug)]
+pub struct CommitLog {
+    pub name: String,
+    number_of_partitions: usize,
+    pub senders: Vec<Sender<InternalMessage>>,
+}
+
+impl CommitLog {
+    pub fn new(
+        name: String,
+        number_of_partitions: usize,
+    ) -> (CommitLog, Vec<Receiver<InternalMessage>>) {
+        let mut senders = Vec::new();
+        let mut receivers = Vec::new();
+        for _ in 0..number_of_partitions {
+            // TODO Revisit buffer size(or make it configurable).
+            let (tx, rx) = mpsc::channel(100);
+            senders.push(tx);
+            receivers.push(rx);
+        }
+        (
+            CommitLog {
+                name,
+                number_of_partitions,
+                senders,
+            },
+            receivers,
+        )
+    }
+
+    pub async fn create_log_files(&self) -> Result<(), &str> {
+        let rog_home = match env::var("ROG_HOME") {
+            Ok(path) => path,
+            Err(_) => return Err("ROG_HOME environment variable not set"),
+        };
+
+        match fs::create_dir_all(format!("{rog_home}/{}", self.name)).await {
+            Ok(_) => debug!("Successfully created rog and log directories"),
+            Err(e) => {
+                error!("Unable to create rog and log directory {e}");
+                return Err("Unable to create rog and log directory");
+            }
+        }
+
+        for partition in 0..self.number_of_partitions {
+            match File::create(format!("{rog_home}/{}/{partition}.log", self.name)).await {
+                Ok(_) => debug!("Successfully created log file for partition {partition}"),
+                Err(e) => {
+                    error!("Unable to create log file for partition {partition} {e}");
+                    return Err("Unable to create log file for partition {partition}");
+                }
+            }
+        }
+
+        Ok(())
+    }
+}
+
+/// Initialize a list a receivers to run in background tasks.
+pub fn setup_receivers(receivers: Vec<Receiver<InternalMessage>>) {
+    for mut receiver in receivers {
+        tokio::spawn(async move {
+            // TODO Micro-batching
+            while let Some(internal_message) = receiver.recv().await {
+                let partition = internal_message.partition;
+                let log_name = internal_message.log_name.clone();
+                let message = Message::new(internal_message);
+
+                let encoded = bincode::serialize(&message).unwrap();
+                let rog_home = match env::var("ROG_HOME") {
+                    Ok(path) => path,
+                    Err(_) => panic!("ROG_HOME enrivonment variable not set"),
+                };
+
+                let log_file_name = format!("{rog_home}/{}/{}.log", log_name, partition);
+                let result = OpenOptions::new().append(true).open(log_file_name).await;
+                let mut log_file = match result {
+                    Ok(file) => file,
+                    Err(e) => {
+                        panic!("Unable to open log file for writing {e}");
+                    }
+                };
+                log_file.write_all(&encoded).await.unwrap();
+                debug!(
+                    "Received message {:?} on partition {:?} with timestamp {:?}",
+                    message.data, partition, message.timestamp
+                );
+            }
+        });
+    }
+}
+
+#[derive(Debug)]
+pub struct InternalMessage {
+    log_name: String,
+    partition: usize,
+    data: BytesMut,
+    timestamp: SystemTime,
+}
+
+impl InternalMessage {
+    pub fn new(log_name: String, partition: usize, data: BytesMut) -> InternalMessage {
+        InternalMessage {
+            log_name,
+            partition,
+            data,
+            timestamp: SystemTime::now(),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Message {
+    data: BytesMut,
+    timestamp: SystemTime,
+}
+
+impl Message {
+    pub fn new(internal_message: InternalMessage) -> Message {
+        Message {
+            data: internal_message.data,
+            timestamp: internal_message.timestamp,
+        }
+    }
+}
+
+pub type Logs = Arc<RwLock<HashMap<String, CommitLog>>>;
+
+#[cfg(test)]
+mod tests {
+    use crate::log::*;
+
+    #[test]
+    fn create_new_commit_log() {
+        let log_name = "log.name".to_string();
+        let partitions = 10;
+
+        let (commit_log, receivers) = CommitLog::new(log_name.clone(), partitions);
+
+        assert_eq!(receivers.len(), partitions);
+        assert_eq!(commit_log.senders.len(), partitions);
+        assert_eq!(commit_log.name, log_name);
+        assert_eq!(commit_log.number_of_partitions, partitions);
+    }
+}
