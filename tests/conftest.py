c539d43047e7f9a2f2585ac3c9911b537a53d8db
Edison Neto <edison.aguiar.neto@gmail.com>
2023-08-13 17:53:26 -0300

    Add a flag on test to run againt local rog server
    This helps to run a test on a "local" server to see the server logs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,10 +5,14 @@ from rog_setup import kill_rog_server, setup_rog_server
 
 def pytest_addoption(parser):
     parser.addoption("--profile", action="store", default="local")
+    parser.addoption("--setup-server", action="store", default="true")
 
 
 @pytest.fixture(autouse=True)
 def setup_and_teardown_rog_server(request):
-    pid = setup_rog_server(request)
-    yield
-    kill_rog_server(pid)
+    if request.config.getoption("--setup-server") == "true":
+        pid = setup_rog_server(request)
+        yield
+        kill_rog_server(pid)
+    else:
+        yield
